cb09b3231e1a577b41d275027d87b2232d0644b9
cuasso <facu.cuassolo.95@gmail.com>
2021-09-02 21:37:49 -0300

    Se agrega router y mas paginas.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import WeddingPage from './pages/Wedding';
+import React, { Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+import Loading from './components/Loading';
+
 import "./styles.css";
+
+const WeddingPage = React.lazy(() => import('./pages/WeddingPage'))
+const FithteenPage = React.lazy(() => import('./pages/FithteenPage'))
+const BaptismPage = React.lazy(() => import('./pages/BaptismPage'))
+const MainPage = React.lazy(() => import('./pages/MainPage'))
 
 const App = () => {
   return (
     <div className="App">
-      <WeddingPage />
+      <Router>
+        <Suspense fallback={Loading}>
+          <Switch>
+            <Route path='/' exact>
+              <MainPage />
+            </Route>
+            <Route path='/nuestra-boda'>
+              <WeddingPage />
+            </Route>
+            <Route path='/mis-15'>
+              <FithteenPage />
+            </Route>
+            <Route path='/mi-primer-bautismo'>
+              <BaptismPage />
+            </Route>
+            <Route path='*'>
+              <MainPage />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Router>
+
     </div>
   );
 }
---
+++ src/pages/BaptismPage.js
@@ -0,0 +1,5 @@
+const BaptismPage = () => {
+    return <h1>Mis primer Bautismo</h1>
+}
+
+export default BaptismPage
